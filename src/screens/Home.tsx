f253126b72f5e2aa924fcf230f3d524f9a0d1494
Ajaymaurya1008 <ajaykvmaurya@gmail.com>
2024-12-04 17:09:00 +0530

    chore : add login and home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,91 @@
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, FlatList, TouchableOpacity} from 'react-native';
 import React from 'react';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import axios from 'axios';
+import {useQuery} from '@tanstack/react-query';
+
+const foodCategories = [
+  {
+    id: 1,
+    name: 'Lunch',
+    icon: 'pizza-outline',
+  },
+  {
+    id: 2,
+    name: 'Dinner',
+    icon: 'fast-food-outline',
+  },
+  {
+    id: 3,
+    name: 'Breakfast',
+    icon: 'cafe-outline',
+  },
+  {
+    id: 4,
+    name: 'Snacks',
+    icon: 'nutrition-outline',
+  },
+  {
+    id: 5,
+    name: 'Dessert',
+    icon: 'ice-cream-outline',
+  },
+  {
+    id: 6,
+    name: 'Drinks',
+    icon: 'beer-outline',
+  },
+];
 
 export default function Home() {
+  const getRecipes = async () => {
+    const res = await axios.get('https://dummyjson.com/recipes');
+    return res.data;
+  };
+
+  const {data} = useQuery({
+    queryKey: ['RECIPES'],
+    queryFn: getRecipes,
+  });
+
+  console.log(data);
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 bg-neutral-100 px-6 pt-16">
+      <Text className="text-4xl font-bold">Hello {'\n'}Abhishek</Text>
+      <Text className="text-xl text-neutral-600 mt-2 font-normal">
+        What do you want to eat ?
+      </Text>
+      <View className="mt-8 px-4 gap-1 bg-neutral-300 justify-center items-center rounded-lg flex-row">
+        <Feather name="search" size={20} color="gray" />
+        <TextInput
+          placeholder="Search"
+          className="flex-1 placeholder:text-lg placeholder:font-semibold placeholder:text-neutral-500"
+        />
+      </View>
+      <FlatList
+        className="mt-8 border"
+        data={foodCategories}
+        horizontal={true}
+        keyExtractor={item => item.id.toString()}
+        ItemSeparatorComponent={Separator}
+        renderItem={({item}) => (
+          <View className="border">
+            <TouchableOpacity
+              key={item.id}
+              className="size-20 justify-center rounded-lg bg-orange-500 items-center">
+              <Ionicons size={50} name={item.icon} color="white" />
+            </TouchableOpacity>
+            <Text className="text-center   border font-medium mt-1 ">{item.name}</Text>
+          </View>
+        )}
+      />
+      <View className="mt-6">
+        <Text>Today's Special</Text>
+      </View>
     </View>
   );
 }
+
+const Separator = () => <View className="w-4" />;
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,50 @@
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 import React from 'react';
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login</Text>
+    <View className="flex-1 bg-neutral-100 items-center justify-center px-4">
+      <View className="gap-2">
+        <Text className="text-center text-4xl font-bold">Welcome Back</Text>
+        <Text className="text-center font-normal">
+          Login to your existant account
+        </Text>
+      </View>
+      <View className="w-full px-8">
+        <TextInput
+          placeholder="Username"
+          className="mt-4 w-full placeholder:font-medium placeholder:text-neutral-400 rounded-xl px-4 py-5 bg-white"
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          className="mt-4 w-full placeholder:font-medium placeholder:text-neutral-400 rounded-xl px-4 py-5 bg-white"
+        />
+      </View>
+      <Text className="mt-4 text-sm font-semibold text-orange-500 text-right px-8 w-full">
+        Forgot password?
+      </Text>
+      <TouchableOpacity className="py-3 px-10 bg-orange-500 justify-center items-center rounded-xl mt-4">
+        <Text className="text-white text-center font-medium text-xl">
+          LOG IN
+        </Text>
+      </TouchableOpacity>
+      <Text className="mt-4">or connect using</Text>
+      <View className="flex-row gap-4">
+        <TouchableOpacity className="py-2 px-4 justify-center items-center bg-orange-500 rounded-md mt-4">
+          <Text className="text-white text-center font-medium text-base">
+            Google
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="py-2 px-4 justify-center items-center bg-orange-500 rounded-md mt-4">
+          <Text className="text-white text-center font-medium text-base">
+            Facebook
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="mt-8 text-base">
+        Don't have an account? <Text className="text-orange-500 font-semibold">Sign Up</Text>{' '}
+      </Text>
     </View>
   );
 }
